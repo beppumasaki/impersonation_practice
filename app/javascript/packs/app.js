f209dfcd8803d7e5b0cea96678e4cd25544a0bf2
beppumasaki <beppu0304@gmail.com>
2022-03-27 07:43:38 +0900

    Add：録音機能のみ実装

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -1,1 +1,42 @@
-console.log('hello world');
+
+let audioCtx;
+
+const record = document.getElementById("record")
+const stop = document.getElementById("stop")
+
+
+if (navigator.mediaDevices.getUserMedia) {
+    console.log('getUserMedia supported.');
+  
+    const constraints = { audio: true };
+    let chunks = [];
+    
+  
+    let onSuccess = function(stream) {
+      const mediaRecorder = new MediaRecorder(stream);
+
+  
+      record.onclick = function() {
+        mediaRecorder.start();
+        console.log(mediaRecorder.state);
+        console.log("recorder started");
+        record.style.background = "red";
+        stop.disabled = false;
+        record.disabled = true;
+      }
+  
+      stop.onclick = function() {
+        mediaRecorder.stop();
+        console.log(mediaRecorder.state);
+        console.log("recorder stopped");
+        // mediaRecorder.requestData();
+        record.style.background = "";
+        stop.disabled = true;
+        record.disabled = false;
+      }}
+
+      let onError = function(err) {
+        console.log('The following error occured: ' + err);
+      }
+      navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
+    };
