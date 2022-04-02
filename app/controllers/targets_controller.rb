980bbd287ae4301bcfc1732159026aed8bdec8c2
beppumasaki <beppu0304@gmail.com>
2022-04-02 12:29:10 +0900

    Add：お題の新規登録、編集機能の追加

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -8,4 +8,36 @@ class TargetsController < ApplicationController
     @targets = Target.all
   end
 
+  def new
+    @target = Target.new
+  end
+
+  def edit
+    @target = Target.find(params[:id])
+  end
+
+  def update
+    @target = Target.find(params[:id])
+    if @target.update(target_params)
+      redirect_to targets_path
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @target = Target.create(target_params)
+    if @target.save
+      redirect_to targets_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def target_params
+    params.require(:target).permit(:name, :target_voice)
+  end
+
 end
