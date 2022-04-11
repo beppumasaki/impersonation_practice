f4b7ce621c7f9585e21f0c46de9fa2e125214182
beppumasaki <beppu0304@gmail.com>
2022-04-11 09:32:13 +0900

    Add：ログイン機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
   resources :targets do
     resources :results, only: %i[create index show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
+
+  resources :users, only: %i[new create]
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
 end
