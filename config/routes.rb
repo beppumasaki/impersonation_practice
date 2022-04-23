ed71dc21a5498b4324b23b30eb7b370e35ebb89b
beppumasaki <beppu0304@gmail.com>
2022-04-23 23:07:30 +0900

    Add：プロフィール画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
 
-  resources :users, only: %i[new create] do
+  resources :users, only: %i[new create show edit update] do
     resources :results, only: %i[index show edit update destroy] do
       resources :comments, only: %i[create destroy]
       resources :collaborations, only: %i[create destroy new index show]
